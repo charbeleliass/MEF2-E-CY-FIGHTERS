9239464015cc7a6a50045605d0cb53484461b641
charbeleliass <eliascharbel05@hotmail.com>
2025-04-26 11:00:36 +0200

    Update combat.c

--- combat.c
+++ combat.c
@@ -2,6 +2,8 @@
 #include <stdlib.h>
 #include "combat.h"
 #include "structure.h"
+#include "choix.h"
+#include "menu.h"
 
 void afficher_stats() {
     system("clear");
@@ -83,25 +85,53 @@ void vraiatout(Player* p, int a) {
     }
 }
 
-void choix_assets(int equipe[], int nbr_joueur) {
+void choix_assets(int equipe[], int nbr_joueur, Player eq_stats[]) {
     int arme = 0;
     for (int i = 0; i <= nbr_joueur; i++) {
         arme = demanderChoixDansIntervalle("choisir arme : ", 0, 6, VERT);
+
         AffichagePerso *paff = &persos_affichage[equipe[i] - 1];
-        atout(paff, arme);
-        afficherPersonnage(equipe[i] - 1);
+        atout(paff, arme);                 // Modifier l'affichage
+        afficherPersonnage(equipe[i] - 1);  // Afficher les nouvelles stats
+        vraiatout(&eq_stats[i], arme);      // Modifier les vraies stats
     }
 }
 
-void choix_assets_E(int equipe1[], int equipe2[], int nbr_joueur) {
+void choix_assets_E(int equipe1[], int equipe2[], int nbr_joueur, Player eq1_stats[], Player eq2_stats[]) {
     afficher_stats();
-    printf("Au tour de l equipe 1:  \n");
-    choix_assets(equipe1 , nbr_joueur);
+    printf("Au tour de l'équipe 1 :\n");
+    choix_assets(equipe1, nbr_joueur, eq1_stats);
 
-    printf("\n Appuie sur Entrée pour continuer... \n");
+    printf("\nAppuie sur Entrée pour continuer...\n");
     while (getchar() != '\n');
 
     afficher_stats();
-    printf("Au tour de l equipe 2:  \n");
-    choix_assets(equipe2 , nbr_joueur);
+    printf("Au tour de l'équipe 2 :\n");
+    choix_assets(equipe2, nbr_joueur, eq2_stats);
 }
+
+/*
+void stats_combat_E(Player eq_stats[], int nbr_joueur){
+    for (int i = 0; i <= nbr_joueur; i++) {
+        printf("[ %-20s ]", eq_stats[i].name);
+    }
+    printf("\n");
+
+    for (int j = 0; j <= nbr_joueur; j++) {
+        printf("[ ");
+        for (int k = 0; k < eq_stats[j].stats_temp.pv; k++) {
+            printf(">");
+        }
+        for (int x = 0; x < (eq_stats[j].stats_temp.pv_max - eq_stats[j].stats_temp.pv); x++) {
+            printf(" ");
+        }
+        printf(" ] ");
+    }
+}
+
+void debut_combat(Player eq1_stats[], Player eq2_stats[], int nbr_joueur) {
+    stats_combat_E(eq1_stats, nbr_joueur);
+    printf("\n\n_________________________________________________________________________________________\n\n");
+    stats_combat_E(eq2_stats, nbr_joueur);
+}
+*/
