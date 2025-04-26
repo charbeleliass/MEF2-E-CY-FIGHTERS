8052525d4d034b3e95a6e6b58883bb22f57ca8a6
charbeleliass <eliascharbel05@hotmail.com>
2025-04-26 10:56:30 +0200

    Update main.c

--- main.c
+++ main.c
@@ -13,7 +13,33 @@ int main() {
     afficherMenu(&mode, &nbr_joueur);
     afficherTousLesPersos();
     choix_joueur(equipe1, equipe2, nbr_joueur, mode);
-    choix_assets_E(equipe1, equipe2, nbr_joueur);
+    Player eq1_stats[nbr_joueur + 1];
+    Player eq2_stats[nbr_joueur + 1];
+
+    switch (nbr_joueur) {
+        case 1: 
+            eq1_stats[0] = p[equipe1[0] - 1];
+            eq1_stats[1] = p[equipe1[1] - 1];
+            eq2_stats[0] = p[equipe2[0] - 1];
+            eq2_stats[1] = p[equipe2[1] - 1];
+            break;
+
+        case 2:
+            eq1_stats[0] = p[equipe1[0] - 1];
+            eq1_stats[1] = p[equipe1[1] - 1];
+            eq1_stats[2] = p[equipe1[2] - 1];
+            eq2_stats[0] = p[equipe2[0] - 1];
+            eq2_stats[1] = p[equipe2[1] - 1];
+            eq2_stats[2] = p[equipe2[2] - 1];
+            break;
+
+        default:
+            printf("Erreur dans la sélection du nombre de joueurs.\n");
+            exit(1);
+    }
+
+    choix_assets_E(equipe1, equipe2, nbr_joueur, eq1_stats, eq2_stats);
 
     return 0;
 }
+
