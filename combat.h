05e5cb492c91e57ad10ede5e1509ae576b2c6056
charbeleliass <eliascharbel05@hotmail.com>
2025-04-26 10:58:43 +0200

    Update combat.h

--- combat.h
+++ combat.h
@@ -8,7 +8,7 @@
 void afficher_stats();
 void atout(AffichagePerso *p, int a);
 void vraiatout(Player* p, int a);
-void choix_assets(int equipe[], int nbr_joueur);
-void choix_assets_E(int equipe1[], int equipe2[], int nbr_joueur);
+void choix_assets(int equipe[], int nbr_joueur, Player eq_stats[])
+void choix_assets_E(int equipe1[], int equipe2[], int nbr_joueur,Player eq1_stats[], Player eq2_stats[]);
 
 #endif
