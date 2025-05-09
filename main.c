ca91bd571800a8c5d4b55cc697ce1adb89dfd80a
charbeleliass <eliascharbel05@hotmail.com>
2025-05-09 10:40:53 +0200

    Update main.c

--- main.c
+++ main.c
@@ -5,43 +5,37 @@
 #include "choix.h"
 #include "combat.h"
 
+#define NB_PERSOS 12
+
 int main() {
     int mode = 0, nbr_joueur = 0;
     int equipe1[3], equipe2[3];
 
+    Player persos[NB_PERSOS];
+    AffichagePerso affichage[NB_PERSOS];
+
+    // Chargement des données depuis fichiers
+    chargerPersonnages(persos, NB_PERSOS);
+    chargerTechniques(persos, NB_PERSOS);
+    chargerAffichage(affichage, persos, NB_PERSOS);
+
+    // Interface de jeu
     afficherTitre();
     afficherMenu(&mode, &nbr_joueur);
-    afficherTousLesPersos();
-    choix_joueur(equipe1, equipe2, nbr_joueur, mode);
+    afficherTousLesPersos(affichage);
+    choix_joueur(equipe1, equipe2, nbr_joueur, mode, affichage);
+
+    // Préparation des équipes
     Player eq1_stats[nbr_joueur + 1];
     Player eq2_stats[nbr_joueur + 1];
-
-    switch (nbr_joueur) {
-        case 1: 
-            eq1_stats[0] = p[equipe1[0] - 1];
-            eq1_stats[1] = p[equipe1[1] - 1];
-            eq2_stats[0] = p[equipe2[0] - 1];
-            eq2_stats[1] = p[equipe2[1] - 1];
-            break;
-
-        case 2:
-            eq1_stats[0] = p[equipe1[0] - 1];
-            eq1_stats[1] = p[equipe1[1] - 1];
-            eq1_stats[2] = p[equipe1[2] - 1];
-            eq2_stats[0] = p[equipe2[0] - 1];
-            eq2_stats[1] = p[equipe2[1] - 1];
-            eq2_stats[2] = p[equipe2[2] - 1];
-            break;
-
-        default:
-            printf("Erreur dans la sélection du nombre de joueurs.\n");
-            exit(1);
+    for (int i = 0; i <= nbr_joueur; i++) {
+        eq1_stats[i] = persos[equipe1[i] - 1];
+        eq2_stats[i] = persos[equipe2[i] - 1];
     }
 
-    choix_assets_E(equipe1, equipe2, nbr_joueur, eq1_stats, eq2_stats);
+    // Choix d’assets (armes) + Combat
+    choix_assets_E(equipe1, equipe2, nbr_joueur, eq1_stats, eq2_stats, affichage);
+    combat(eq1_stats, eq2_stats, nbr_joueur, affichage);
 
-    combat(eq1_stats, eq2_stats, nbr_joueur);
-    
     return 0;
 }
-
