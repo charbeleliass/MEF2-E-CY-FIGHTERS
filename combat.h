3115cde435bc7864ea9e85ff1d0110c1d5af8d85
charbeleliass <eliascharbel05@hotmail.com>
2025-05-09 10:41:31 +0200

    Update combat.h

--- combat.h
+++ combat.h
@@ -2,16 +2,16 @@
 #define COMBAT_H
 
 #include "structure.h"
-#include "choix.h"
-#include "menu.h"
 
 void afficher_stats();
 void atout(AffichagePerso *p, int a);
 void vraiatout(Player* p, int a);
-void choix_assets(int equipe[], int nbr_joueur, Player eq_stats[]);
-void choix_assets_E(int equipe1[], int equipe2[], int nbr_joueur,Player eq1_stats[], Player eq2_stats[]);
+void choix_assets(int equipe[], int nbr_joueur, Player eq_stats[], AffichagePerso persos_affichage[]);
+void choix_assets_E(int equipe1[], int equipe2[], int nbr_joueur, Player eq1_stats[], Player eq2_stats[], AffichagePerso persos_affichage[]);
 void afficher_etat_combat(Player eq1[], Player eq2[], int joueur_actuel, int equipe_joueur, int nbr_joueur);
-void combat(Player eq1_stats[], Player eq2_stats[], int nbr_joueur);
+void afficher_emojis_effets(Player p);
+
+void combat(Player eq1_stats[], Player eq2_stats[], int nbr_joueur, AffichagePerso affichage[]);
 
 void attaque_classique(Player* attaquant, Player ennemis[], int nbr_joueur);
 void utiliser_competence(Player* lanceur, Player ennemis[], int nbr_joueur);
